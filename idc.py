46f6d06a55cdc5b521aa441bb2a28c915ec5961b
vgntzz <felenode@gmail.com>
2022-03-30 21:37:56 -0300

    isthismysanity000

--- idc.py
+++ idc.py
@@ -21,3 +21,18 @@ print(model)
 
 def implicit_equals(*ab):return not[*{*ab}][1:]
 for*k,in[(0**0,1,1>0),((),()),(0,0<0),((1,2),)*3,(1,2,3)]:print(implicit_equals(*k))
+
+import dis,types
+a=lambda:print(*[*map(chr,[104,101,108,108,111])],sep='')
+b = types.CodeType(0,0,0,0,6,67,bytes([116,0,103,0,116,1,116,2,103,0,100,1,162,1,131,2,162,1,100,2,100,3,105,1,142,1,83,0]),(None, [104, 101, 108, 108, 111], 'sep', ''),('print','map','chr'),(),'','',1,bytes([0,0]))
+print(a.__code__.co_argcount == b.co_argcount)
+print(a.__code__.co_nlocals == b.co_nlocals)
+print(a.__code__.co_varnames == b.co_varnames)
+print(a.__code__.co_flags == b.co_flags)
+print(a.__code__.co_stacksize == b.co_stacksize)
+print(a.__code__.co_consts, b.co_consts)
+print(a.__code__.co_names == b.co_names)
+print(a.__code__.co_varnames == b.co_varnames)
+print(b.co_code == a.__code__.co_code)
+a()
+exec(b)
